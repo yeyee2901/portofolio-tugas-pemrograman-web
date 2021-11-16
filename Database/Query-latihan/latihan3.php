f543a5738bdfc8b1f0c4ac4e96cc46563bf95616
yeyee2901 <gabriel777sh@gmail.com>
2021-11-16 22:46:30 +0700

    ADD: latihan database 2 (SELECT)

--- Database/Query-latihan/latihan3.php
+++ Database/Query-latihan/latihan3.php
@@ -11,30 +11,9 @@ $conn = mysqli_connect($host, $user, $pass, $database);
 $table_name = "dokter";
 $q = "SELECT * FROM $table_name";
 
-// simpan result
+// simpan result (error || result obj)
 $result = mysqli_query($conn, $q);
 
-
-// tampilkan hasil query SELECT dalam bentuk array asosiatif
-// jika tidak ada ERROR
-if ($result) {
-  $result = mysqli_fetch_all($result, MYSQLI_ASSOC);
-  echo "
-  <h1 style='color: green;'>
-    hasil SELECT: <br>
-  </h1>
-";
-  var_dump($result);
-}
-
-// jika terjadi ERROR pada database
-else {
-  echo "
-  <h1 style='color: red;'>
-    Terjadi ERROR! 
-  </h1>";
-  echo mysqli_error($conn);
-}
 ?>
 
 <!DOCTYPE html>
@@ -44,33 +23,58 @@ else {
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width">
   <title>Percobaan SELECT</title>
+  <style type="text/css" media="screen">
+    table,td,tr {
+      border: 1px solid black;
+      border-collapse: collapse;
+      padding: 2%;
+    }
+
+  </style>
 
 </head>
 
 <body>
   <table>
     <thead>
-      <tr>ID dokter</tr>
-      <tr>Nama</tr>
-      <tr>Alamat</tr>
-      <tr>Domisili</tr>
-      <tr>Tanda Pengenal</tr>
-      <tr>Keahlian</tr>
+      <td>
+        <h3>ID dokter</h3>
+      </td>
+      <td>
+        <h3>Nama</h3>
+      </td>
+      <td>
+        <h3>Alamat</h3>
+      </td>
+      <td>
+        <h3>Domisili</h3>
+      </td>
+      <td>
+        <h3>Tanda Pengenal</h3>
+      </td>
+      <td>
+        <h3>Keahlian</h3>
+      </td>
     </thead>
 
     <tbody>
 
-      <?php foreach ($result as $dokter) : ?>
-
-        <td> <?= $dokter["id_dokter"] ?> </td>
-        <td> <?= $dokter["nama"] ?> </td>
-        <td> <?= $dokter["alamat"] ?> </td>
-        <td> <?= $dokter["kota"] ?> </td>
-        <td> <?= $dokter["id_pengenal"] ?> </td>
-        <td> <?= $dokter["keahlian"] ?> </td>
-
-      <?php endforeach; ?>
-
+      <!-- Cek result -->
+      <?php if ($result) : ?>
+        <?php $result = mysqli_fetch_all($result, MYSQLI_ASSOC); ?>
+        <?php foreach ($result as $dokter) : ?>
+          <tr>
+            <td> <?= $dokter["id_dokter"] ?> </td>
+            <td> <?= $dokter["nama"] ?> </td>
+            <td> <?= $dokter["alamat"] ?> </td>
+            <td> <?= $dokter["kota"] ?> </td>
+            <td> <?= $dokter["id_pengenal"] ?> </td>
+            <td> <?= $dokter["keahlian"] ?> </td>
+          </tr>
+        <?php endforeach; ?>
+      <?php else : ?>
+        <?= mysqli_error($conn); ?>
+      <?php endif; ?>
     </tbody>
 
   </table>
